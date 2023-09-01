e44403a62263860cac38d55b4dcf4fad0392acc2
Zach Mueller <muellerzr@gmail.com>
2023-09-01 05:44:31 -0400

    Big refactor

---
+++ src/app.py
@@ -0,0 +1,74 @@
+import gradio as gr
+import pandas as pd
+
+from .hub_utils import check_for_discussion, report_results
+from .model_utils import calculate_memory, get_model
+
+
+# We need to store them as globals because gradio doesn't have a way for us to pass them in to the button
+MODEL = None
+
+
+def get_results(model_name: str, library: str, options: list, access_token: str):
+    global MODEL
+    MODEL = get_model(model_name, library, access_token)
+    has_discussion = check_for_discussion(model_name)
+    title = f"## Memory usage for '{model_name}'"
+    data = calculate_memory(MODEL, options)
+    return [title, gr.update(visible=True, value=pd.DataFrame(data)), gr.update(visible=not has_discussion)]
+
+
+with gr.Blocks() as demo:
+    with gr.Column():
+        gr.Markdown(
+            """<img src="https://huggingface.co/spaces/hf-accelerate/model-memory-usage/resolve/main/measure_model_size.png" style="float: left;" width="250" height="250"><h1>🤗 Model Memory Calculator</h1>
+
+    This tool will help you calculate how much vRAM is needed to train and perform big model inference
+    on a model hosted on the 🤗 Hugging Face Hub. The minimum recommended vRAM needed for a model
+    is denoted as the size of the "largest layer", and training of a model is roughly 4x its size (for Adam).
+
+    These calculations are accurate within a few percent at most, such as `bert-base-cased` being 413.68 MB and the calculator estimating 413.18 MB.
+
+    When performing inference, expect to add up to an additional 20% to this as found by [EleutherAI](https://blog.eleuther.ai/transformer-math/). 
+    More tests will be performed in the future to get a more accurate benchmark for each model.
+
+    Currently this tool supports all models hosted that use `transformers` and `timm`.
+
+    To use this tool pass in the URL or model name of the model you want to calculate the memory usage for,
+    select which framework it originates from ("auto" will try and detect it from the model metadata), and
+    what precisions you want to use."""
+        )
+        out_text = gr.Markdown()
+        out = gr.DataFrame(
+            headers=["dtype", "Largest Layer", "Total Size", "Training using Adam"],
+            interactive=False,
+            visible=False,
+        )
+        with gr.Row():
+            inp = gr.Textbox(label="Model Name or URL", value="bert-base-cased")
+        with gr.Row():
+            library = gr.Radio(["auto", "transformers", "timm"], label="Library", value="auto")
+            options = gr.CheckboxGroup(
+                ["float32", "float16/bfloat16", "int8", "int4"],
+                value="float32",
+                label="Model Precision",
+            )
+            access_token = gr.Textbox(label="API Token", placeholder="Optional (for gated models)")
+        with gr.Row():
+            btn = gr.Button("Calculate Memory Usage")
+            post_to_hub = gr.Button(
+                value="Report results in this model repo's discussions!\n(Will open in a new tab)", visible=False
+            )
+
+    btn.click(
+        get_results,
+        inputs=[inp, library, options, access_token],
+        outputs=[out_text, out, post_to_hub],
+    )
+
+    post_to_hub.click(report_results, inputs=[inp, library, access_token]).then(
+        lambda: gr.Button.update(visible=False), outputs=post_to_hub
+    )
+
+
+demo.launch()
---
+++ src/hub_utils.py
@@ -0,0 +1,62 @@
+# Utilities related to searching and posting on the Hub
+import os
+import webbrowser
+from urllib.parse import urlparse
+
+import pandas as pd
+from huggingface_hub import HfApi
+
+from .model_utils import calculate_memory, get_model
+
+
+def extract_from_url(name: str):
+    "Checks if `name` is a URL, and if so converts it to a model name"
+    is_url = False
+    try:
+        result = urlparse(name)
+        is_url = all([result.scheme, result.netloc])
+    except Exception:
+        is_url = False
+    # Pass through if not a URL
+    if not is_url:
+        return name
+    else:
+        path = result.path
+        return path[1:]
+
+
+def check_for_discussion(model_name: str):
+    "Checks if an automated discussion has been opened on the model by `model-sizer-bot`"
+    api = HfApi(token=os.environ.get("HUGGINGFACE_API_LOGIN", None))
+    discussions = list(api.get_repo_discussions(model_name))
+    return any(
+        discussion.title == "[AUTOMATED] Model Memory Requirements" and discussion.author == "model-sizer-bot"
+        for discussion in discussions
+    )
+
+
+def report_results(model_name, library, access_token):
+    "Reports the results of a memory calculation to the model's discussion page, and opens a new tab to it afterwards"
+    model = get_model(model_name, library, access_token)
+    data = calculate_memory(model, ["fp32", "fp16", "int8", "int4"])
+    minimum = data[0]
+    data = pd.DataFrame(data).to_markdown(index=False)
+
+    post = f"""# Model Memory Requirements\n
+
+You will need about {minimum[1]} VRAM to load this model for inference, and {minimum[3]} VRAM to train it using Adam.
+    
+These calculations were measured from the [Model Memory Utility Space](https://hf.co/spaces/hf-accelerate/model-memory-utility) on the Hub.
+    
+The minimum recommended vRAM needed for this model assumes using [Accelerate or `device_map="auto"`](https://huggingface.co/docs/accelerate/usage_guides/big_modeling) and is denoted by the size of the "largest layer". 
+When performing inference, expect to add up to an additional 20% to this, as found by [EleutherAI](https://blog.eleuther.ai/transformer-math/). More tests will be performed in the future to get a more accurate benchmark for each model.
+
+When training with `Adam`, you can expect roughly 4x the reported results to be used. (1x for the model, 1x for the gradients, and 2x for the optimizer).
+
+## Results:
+
+{data}
+"""
+    api = HfApi(token=os.environ.get("HUGGINGFACE_API_LOGIN", None))
+    discussion = api.create_discussion(model_name, "[AUTOMATED] Model Memory Requirements", description=post)
+    webbrowser.open_new_tab(discussion.url)
